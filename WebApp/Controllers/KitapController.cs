c3bea8baac078e8b7ed8a61ade326bcfce2381db
Erkan Uluocak <erkan.uluocak@gmail.com>
2022-05-28 14:44:13 +0300

    Add project files.

---
+++ WebApp/Controllers/KitapController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using WebApp.Data;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class KitapController : Controller
+    {
+        ApplicationDbContext _db;
+        UserManager<Uye> _userMan;
+        public KitapController(ApplicationDbContext db, UserManager<Uye> userMan)
+        {
+            _db = db;
+            //db.Database.EnsureDeleted();
+            db.Database.EnsureCreated();
+
+            // _userMan = userMan;
+            // AdminYap();
+
+
+        }
+
+        private async void AdminYap() {
+            Uye uye = _userMan.FindByIdAsync("1").Result;
+            await _userMan.AddToRoleAsync(uye, "Admin");
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
